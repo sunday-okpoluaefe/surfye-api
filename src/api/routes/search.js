ea36e158f3d80e543e61784ec40cb5ef5458ce21
Sunday Onoriode <sunday-onoriode@Sunday-Onoriode-MacBook-Pro.local>
2022-10-08 19:50:51 +0100

    Fixed bug with my posts

--- src/api/routes/search.js
+++ src/api/routes/search.js
@@ -6,6 +6,6 @@ const router = express.Router();
 const middlewares = require('../providers/middlewares');
 const { SearchController } = require('../providers/controllers');
 
-router.get('/', middlewares.passport.authenticate, middlewares.validateRequest, middlewares.async(SearchController.search));
+router.get('/', middlewares.passport.checkAuthorization, middlewares.validateRequest, middlewares.async(SearchController.search));
 
 module.exports.SearchRoutes = router;
